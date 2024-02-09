4f3c56769dad17c3327a2ea0e627c60336b97ea0
uikola <ugulaev806@yandex.com>
2024-02-09 13:19:01 +0400

    Добавлена функция инициализации базы данных для калькулятора.

---
+++ calculator/internal/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"github.com/Uikola/yandexDAEC/calculator/internal/config"
+	"github.com/rs/zerolog"
+)
+
+// InitDB инициализирует инстанцию базы данных.
+func InitDB(cfg *config.Config, log zerolog.Logger) *sql.DB {
+	db, err := sql.Open(cfg.DriverName, cfg.ConnString)
+	if err != nil {
+		log.Info().Err(err).Msg("failed to connect to the database")
+		return nil
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Info().Err(err).Msg("failed to ping the database")
+		return nil
+	}
+	return db
+}
