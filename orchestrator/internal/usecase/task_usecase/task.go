cdea9039acdb191130a9da9a31d2e92d2796bbea
uikola <ugulaev806@yandex.com>
2024-02-09 12:36:01 +0400

    Реализован юс кейс для задач.

---
+++ orchestrator/internal/usecase/task_usecase/task.go
@@ -0,0 +1,94 @@
+package task_usecase
+
+import (
+	"context"
+	"fmt"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/errorz"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka/consumer"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka/producer"
+	"math/rand"
+	"strconv"
+)
+
+// AddTask отправляет арифметическое выражение в топик "expressions" и вызывает метод репозитория AddTask.
+func (uc UseCaseImpl) AddTask(ctx context.Context, task entity.Task) (int64, error) {
+	exists, err := uc.taskRepository.Exists(ctx, task.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	if exists {
+		return 0, errorz.ErrTaskAlreadyExists
+	}
+
+	id, err := uc.taskRepository.AddTask(ctx, task)
+	if err != nil {
+		return 0, err
+	}
+
+	resources, err := uc.cResourceRepository.ListFreeCResource(ctx)
+	if err != nil {
+		return 0, err
+	}
+	if len(resources) == 0 {
+		return 0, errorz.ErrNoAvailableResources
+	}
+
+	resource := resources[rand.Intn(len(resources))]
+	err = uc.cResourceRepository.OccupyCResource(ctx, resource.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	err = uc.taskRepository.UpdateComputingResource(ctx, id, resource.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	p, err := producer.NewKafkaProducer(kafka.Brokers)
+	if err != nil {
+		return 0, err
+	}
+	msg := fmt.Sprintf("%s:%d", task.Expression, id)
+	p.SendMessage(kafka.ExpressionTopic, msg, resource.Name)
+	return id, nil
+}
+
+// ListTask вызывает метод репозитория ListTask.
+func (uc UseCaseImpl) ListTask(ctx context.Context, limit, offset int) ([]entity.Task, error) {
+	return uc.taskRepository.ListTask(ctx, limit, offset)
+}
+
+// GetTask вызывает метод репозитория GetTask.
+func (uc UseCaseImpl) GetTask(ctx context.Context, id int64) (entity.Task, error) {
+	return uc.taskRepository.GetTask(ctx, id)
+}
+
+func (uc UseCaseImpl) GetResult(ctx context.Context, id int64) (string, error) {
+	task, err := uc.taskRepository.GetTask(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	cResource, err := uc.cResourceRepository.GetCResource(ctx, *task.CalculatedBy)
+	if err != nil {
+		return "", err
+	}
+	res, err := consumer.StartResultConsumer(strconv.FormatInt(id, 10))
+	if err != nil {
+		return "", err
+	}
+	err = uc.taskRepository.CompleteTask(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	err = uc.cResourceRepository.FreeCResource(ctx, cResource.ID)
+	if err != nil {
+		return "", nil
+	}
+
+	return res, nil
+}
---
+++ orchestrator/internal/usecase/task_usecase/usecase.go
@@ -0,0 +1,38 @@
+package task_usecase
+
+import (
+	"context"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+)
+
+// TaskRepository интерфейс репозитория для работы с задачами.
+type TaskRepository interface {
+	AddTask(ctx context.Context, task entity.Task) (int64, error)
+	Exists(ctx context.Context, taskID int64) (bool, error)
+	ListTask(ctx context.Context, limit, offset int) ([]entity.Task, error)
+	GetTask(ctx context.Context, id int64) (entity.Task, error)
+	UpdateComputingResource(ctx context.Context, taskID, cResourceID int64) error
+	CompleteTask(ctx context.Context, taskID int64) error
+}
+
+// CResourceRepository интерфейс репозитория для работы с вычислительными ресурсами.
+type CResourceRepository interface {
+	ListFreeCResource(ctx context.Context) ([]entity.ComputingResource, error)
+	OccupyCResource(ctx context.Context, cResourceID int64) error
+	FreeCResource(ctx context.Context, cResourceID int64) error
+	GetCResource(ctx context.Context, id int64) (entity.ComputingResource, error)
+}
+
+// UseCaseImpl структура для работы с имплементацией юс кейса задач.
+type UseCaseImpl struct {
+	taskRepository      TaskRepository
+	cResourceRepository CResourceRepository
+}
+
+// New возвращает новую инстанцию UseCaseImpl.
+func New(taskRepository TaskRepository, cResourceRepository CResourceRepository) *UseCaseImpl {
+	return &UseCaseImpl{
+		taskRepository:      taskRepository,
+		cResourceRepository: cResourceRepository,
+	}
+}
