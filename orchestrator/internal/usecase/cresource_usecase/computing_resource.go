b03b55c72f3d4014a8e732385453527b0538e768
uikola <ugulaev806@yandex.com>
2024-02-09 12:34:07 +0400

    Реализован юс кейс для вычислительных ресурсов.

---
+++ orchestrator/internal/usecase/cresource_usecase/computing_resource.go
@@ -0,0 +1,75 @@
+package cresource_usecase
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka/consumer"
+	"github.com/Uikola/yandexDAEC/orchestrator/pkg/kafka/producer"
+	"golang.org/x/sync/errgroup"
+	"math/rand"
+	"time"
+)
+
+// AddCResource в отдельной горутине вызывает прослушку heartbeat сообщений(в случае ошибки удаляет
+// созданную запись, вызвав метод DeleteCResource) и вызывает метод репозитория AddCResource.
+func (uc UseCaseImpl) AddCResource(ctx context.Context, cResource entity.ComputingResource) error {
+	g := new(errgroup.Group)
+	g.Go(func() error {
+		ctx := context.Background()
+		err := consumer.StartHeartbeatConsumer(cResource.Name)
+		if err != nil {
+			createdCResource, err := uc.cResourceRepository.GetCResourceByName(ctx, cResource.Name)
+			if err != nil {
+				return err
+			}
+
+			if createdCResource.Occupied {
+				fmt.Println("try to resend message")
+				task, err := uc.taskRepository.GetTaskByCResource(ctx, createdCResource.ID)
+				if err != nil {
+					return err
+				}
+
+				resources, err := uc.cResourceRepository.ListFreeCResource(ctx)
+				switch {
+				case errors.Is(err, sql.ErrNoRows):
+					time.Sleep(50 * time.Second)
+					resources, err = uc.cResourceRepository.ListFreeCResource(ctx)
+				case err != nil:
+					return err
+				}
+
+				resource := resources[rand.Intn(len(resources))]
+				err = uc.cResourceRepository.OccupyCResource(ctx, resource.ID)
+				if err != nil {
+					return err
+				}
+
+				err = uc.taskRepository.UpdateComputingResource(ctx, task.ID, resource.ID)
+				if err != nil {
+					return err
+				}
+
+				p, err := producer.NewKafkaProducer(kafka.Brokers)
+				if err != nil {
+					return err
+				}
+
+				msg := fmt.Sprintf("%s:%d", task.Expression, task.ID)
+				p.SendMessage(kafka.ExpressionTopic, msg, resource.Name)
+			}
+			err = uc.cResourceRepository.DeleteCResource(context.Background(), cResource.Name)
+			fmt.Println(cResource.Name)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return uc.cResourceRepository.AddCResource(ctx, cResource)
+}
---
+++ orchestrator/internal/usecase/cresource_usecase/usecase.go
@@ -0,0 +1,36 @@
+package cresource_usecase
+
+import (
+	"context"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+)
+
+// CResourceRepository интерфейс репозитория для работы с вычислительными ресурсами.
+type CResourceRepository interface {
+	AddCResource(ctx context.Context, cResource entity.ComputingResource) error
+	DeleteCResource(ctx context.Context, cResourceName string) error
+	ListFreeCResource(ctx context.Context) ([]entity.ComputingResource, error)
+	OccupyCResource(ctx context.Context, cResourceID int64) error
+	GetCResourceByName(ctx context.Context, name string) (entity.ComputingResource, error)
+}
+
+// TaskRepository интерфейс репозитория для работы с задачами.
+type TaskRepository interface {
+	AddTask(ctx context.Context, task entity.Task) (int64, error)
+	ListTask(ctx context.Context, limit, offset int) ([]entity.Task, error)
+	GetTask(ctx context.Context, id int64) (entity.Task, error)
+	UpdateComputingResource(ctx context.Context, taskID, cResourceID int64) error
+	CompleteTask(ctx context.Context, taskID int64) error
+	GetTaskByCResource(ctx context.Context, cResourceID int64) (entity.Task, error)
+}
+
+// UseCaseImpl структура для работы с имплементацией юс кейса вычислительных ресурсов.
+type UseCaseImpl struct {
+	cResourceRepository CResourceRepository
+	taskRepository      TaskRepository
+}
+
+// New возвращает новую инстанцию UseCaseImpl.
+func New(cResourceRepository CResourceRepository, taskRepository TaskRepository) *UseCaseImpl {
+	return &UseCaseImpl{cResourceRepository: cResourceRepository, taskRepository: taskRepository}
+}
