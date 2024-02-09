6ed783591d945d4465baca51c5c9b1b9157483eb
uikola <ugulaev806@yandex.com>
2024-02-09 12:07:15 +0400

    Добавлено подключение к базе данных.

---
+++ orchestrator/internal/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/config"
+	"github.com/rs/zerolog/log"
+)
+
+// InitDB инициализирует инстанцию базы данных.
+func InitDB(cfg *config.Config) *sql.DB {
+	db, err := sql.Open(cfg.DriverName, cfg.ConnString)
+	if err != nil {
+		log.Info().Err(err).Msg("failed to connect to the database")
+		return nil
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Info().Err(err).Msg("failed to ping the database")
+		return nil
+	}
+	return db
+}
