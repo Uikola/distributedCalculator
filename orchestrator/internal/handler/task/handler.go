4651b08f02b7ef653c63b216cf935a5118ad8d62
uikola <ugulaev806@yandex.com>
2024-02-09 12:39:24 +0400

    Реализован хендлер получения задачи по её идентификатору.

---
+++ orchestrator/internal/handler/task/handler.go
@@ -0,0 +1,26 @@
+package task
+
+import (
+	"context"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+)
+
+// UseCase интерфейс для взаимодействия с юс кейсом задач.
+type UseCase interface {
+	AddTask(ctx context.Context, task entity.Task) (int64, error)
+	ListTask(ctx context.Context, limit, offset int) ([]entity.Task, error)
+	GetTask(ctx context.Context, id int64) (entity.Task, error)
+	GetResult(ctx context.Context, id int64) (string, error)
+}
+
+// Handler структура для работы с task handler.
+type Handler struct {
+	useCase UseCase
+}
+
+// New возвращает инстанцию Handler.
+func New(useCase UseCase) *Handler {
+	return &Handler{
+		useCase: useCase,
+	}
+}
