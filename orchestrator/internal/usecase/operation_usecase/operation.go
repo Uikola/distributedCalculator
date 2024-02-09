58889001bfc06bf0813b4fcbb91c24ba2e8e19ee
uikola <ugulaev806@yandex.com>
2024-02-09 12:35:32 +0400

    Реализован юс кейс для операций.

---
+++ orchestrator/internal/usecase/operation_usecase/operation.go
@@ -0,0 +1,16 @@
+package operation_usecase
+
+import (
+	"context"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+)
+
+// ListOperation вызывает метод репозитория ListOperation.
+func (uc UseCaseImpl) ListOperation(ctx context.Context) ([]entity.Operation, error) {
+	return uc.repository.ListOperation(ctx)
+}
+
+// UpdateOperationTime вызывает метод репозитория UpdateOperationTime.
+func (uc UseCaseImpl) UpdateOperationTime(ctx context.Context, operation entity.Operation) error {
+	return uc.repository.UpdateOperationTime(ctx, operation)
+}
---
+++ orchestrator/internal/usecase/operation_usecase/usecase.go
@@ -0,0 +1,22 @@
+package operation_usecase
+
+import (
+	"context"
+	"github.com/Uikola/yandexDAEC/orchestrator/internal/entity"
+)
+
+// OperationRepository интерфейс репозитория для работы с арифметическими операциями.
+type OperationRepository interface {
+	ListOperation(ctx context.Context) ([]entity.Operation, error)
+	UpdateOperationTime(ctx context.Context, operation entity.Operation) error
+}
+
+// UseCaseImpl структура для работы с имплементацией юс кейса арифметических операций.
+type UseCaseImpl struct {
+	repository OperationRepository
+}
+
+// New возвращает новую инстанцию UseCaseImpl.
+func New(repo OperationRepository) *UseCaseImpl {
+	return &UseCaseImpl{repository: repo}
+}
